8d55d0ee2c30449f88e26949f51af7cab4e8ef7a
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2024-05-30 16:21:07 -0500

    removed the userdata script file

--- userdata.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-yum update -y
-yum install -y docker
-systemctl enable docker
-systemctl start docker
-docker login -u tevindeale -p <pw>
-docker run -itd --network host --name rbapi --restart always tevindeale/rocketbankapi:amd64-1.0.8
